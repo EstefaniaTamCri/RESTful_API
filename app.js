9e6a321e72eef4b391939468945d1dde90520ba1
EstefaniaTamCri <estefania_92_9@hotmail.com>
2023-06-19 20:28:29 +0200

    add userModel

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //configuracion inical de la aplicacion
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const PORT = 8000;
 
 //creamos la aplicacion de express
@@ -10,30 +10,48 @@ const app = express();
 app.use(express.json());
 
 //damos permiso a la aplicacion para que pueda acceder a la base de datos
-require('dotenv').config();
+require("dotenv").config();
 
 //obtenemos la cadena de conexion de la base de datos desde las variables de entorno (fichero.env)
 const mongoUrl = process.env.DATABASE_URL_DEV;
 
 //Configuracion con mongodb
 //useNewUrlParser: true -> le indica a monoose que utilice el nuevo analizador de URL de la cadena de conexcion
-mongoose.connect(mongoUrl, { useNewUrlParser: true});
+mongoose.connect(mongoUrl, { useNewUrlParser: true });
 
 //guardamos la conexion con mongoose
 const db = mongoose.connection;
 
 //verificamos que la conexion se ha realizado correctamente
-db.on('error', () => {console.error('Error:', error)});
+db.on("error", () => {
+  console.error("Error:", error);
+});
 
 //nos indica que se establecido la conexion correctamente
-db.once('connected', () => {console.log('Success connect')});
+db.once("connected", () => {
+  console.log("Success connect");
+});
 
 //nos indica que la conexion se ha desconectado
-db.on('disconnected', () => {console.log('Mongoose conecction is disconnected')});
+db.on("disconnected", () => {
+  console.log("Mongoose conecction is disconnected");
+});
 
-const users = require('./Controller/userController');
-app.use('/users', users); 
+const users = require("./Controller/userController");
+const { type } = require("os");
+app.use("/users", users);
+
+const ejemplo = {
+  leadership: "Carismático y líder nato",
+  charisma: "Posee un gran carisma",
+};
+
+console.log(
+  "resultado final ->",
+  ejemplo.contrsuctor === Object &&
+    Object.values(ejemplo).every((element) => typeof element === "string")
+);
 
 app.listen(PORT, () => {
-    console.log(`Server running http://localhost:${PORT}`);
-})
+  console.log(`Server running http://localhost:${PORT}`);
+});
